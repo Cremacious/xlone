a4f66e0a1df17adc96bc73b8c773e60175cbd106
cremacious <chrismackall3@gmail.com>
2025-10-13 14:37:38 -0400

    Added routes and controller for notifications and posts

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
+import userRoutes from './routes/user.route.js';
+import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
 
 dotenv.config();
 
@@ -15,6 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
