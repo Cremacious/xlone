c6b94e7512ea988b91e0ce735cfb12dccdd2c6ca
cremacious <chrismackall3@gmail.com>
2025-10-14 12:16:44 -0400

    Updated UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/auth/login/LoginPage';
+import SignupPage from './pages/auth/signup/SignupPage';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+
 function App() {
   return (
-    <div className="text-black">
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
     </div>
   );
 }
